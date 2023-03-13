b9deb0cd0fe551e80ae3a8510756d409e2755245
k0rsakov <korsakov.iyu@gmail.com>
2023-03-13 10:54:21 +0800

    Изменен проект dag_factory: - Изменен config для генерации новых DAG

---
+++ dags/json_dags/print_something/template_4.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+
+import pendulum
+
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+
+
+args = {
+    'owner': 'k0rsakov',
+    'start_date': datetime(2022, 8, 1, tzinfo=pendulum.timezone('Europe/Moscow')),
+    'catchup': True,
+    'retries': 3,
+    'retry_delay': timedelta(hours=1),
+    'max_active_runs': 1
+}
+
+
+def print_something() -> None:
+    """
+    Печатает текст.
+
+    :return: `None`
+    """
+    print('something_4')
+
+with DAG(
+        dag_id='template_4',
+        schedule_interval='0 10 * * *',
+        default_args=args,
+        tags=['template', 'test'],
+        concurrency=1
+) as dag:
+
+    start = EmptyOperator(
+        task_id='start',
+    )
+
+    print_something = PythonOperator(
+        task_id='print_something',
+        python_callable=print_something,
+    )
+
+    end = EmptyOperator(
+        task_id='end',
+    )
+
+    start >> print_something >> end
---
+++ dags/json_dags/print_something/template_9.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timedelta
+
+import pendulum
+
+from airflow import DAG
+
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+
+
+args = {
+    'owner': 'k0rsakov',
+    'start_date': datetime(2023, 4, 10, tzinfo=pendulum.timezone('Europe/Moscow')),
+    'catchup': True,
+    'retries': 3,
+    'retry_delay': timedelta(hours=1),
+    'max_active_runs': 1
+}
+
+
+def print_something() -> None:
+    """
+    Печатает текст.
+
+    :return: `None`
+    """
+    print('something_9')
+
+with DAG(
+        dag_id='template_9',
+        schedule_interval='10 0 * * *',
+        default_args=args,
+        tags=['template', 'test'],
+        concurrency=1
+) as dag:
+
+    start = EmptyOperator(
+        task_id='start',
+    )
+
+    print_something = PythonOperator(
+        task_id='print_something',
+        python_callable=print_something,
+    )
+
+    end = EmptyOperator(
+        task_id='end',
+    )
+
+    start >> print_something >> end
